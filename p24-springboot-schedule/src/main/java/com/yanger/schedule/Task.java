b265e8cc1db3be943cdeea03f2e77ad991153593
guopengen <wpfysu@163.com>
2019-12-24 20:25:15 +0800

    schedule加入注释说明

--- p24-springboot-schedule/src/main/java/com/yanger/schedule/Task.java
+++ p24-springboot-schedule/src/main/java/com/yanger/schedule/Task.java
@@ -8,6 +8,10 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 /**
+ * 计划任务Scheduled是通过一个线程池实现的。是一个多线程的调度。
+ * SpringBoot会初始化一个线程池，线程池默认大小为1，专门用于执行计划任务。
+ * 每个计划任务启动的时候，都从线程池中获取一个线程执行，如果发生异常，只是执行当前任务的线程发生异常，而不是计划任务调度线程发生异常。
+ * 如果当前定时任务还未执行完成，当相同的定时任务又进入到执行周期时，不会触发新的定时任务
  * @author yanger
  * @description 定时任务执行类
  * @date 2019/10/15
@@ -18,9 +22,15 @@ public class Task {
 
     private static final SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
 
-    @Scheduled(fixedRate = 1000)
+    @Scheduled(cron = "* * * * * ?")
     public void taskOne(){
+        try {
+            Thread.sleep(3000);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         System.out.println("现在时间：" + dateFormat.format(new Date()));
+
     }
 
 }
